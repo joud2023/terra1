ee0f95e908f1cd03e24a0a3525cc29d39c12bf89
joud2023 <vagrant@controlernode>
2023-05-25 00:23:38 +0000

    first commit

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+/*
+  cloud {
+   organization = "VITALIS-TECHNOLOGY"
+
+   workspaces {
+     name = "ansible-terraform"
+   }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
